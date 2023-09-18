378f5d168fb47b4f59557a854b9e3cbaf0d46f31
lijiahao <lij50691@gmail.com>
2023-09-18 16:26:08 +0800

    主题色的配置

--- vite.config.js
+++ vite.config.js
@@ -17,12 +17,26 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [
+        //配置elementPlus采用sass样式配色系统
+        ElementPlusResolver({
+          importStyle: "sass",
+          // directives: true,
+          // version: "2.1.5",
+        }),
+      ],
     }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/element/index.scss" as *;`,
+      },
+    },
+  },
 })
